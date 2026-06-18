1b1297543e38e0bd378af9f25f41a9178d7f057e
Jasmin <maryjasmin5947@gmail.com>
2026-06-19 00:48:34 +0530

    Configure frontend for Render

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_URL =
-  `${process.env.REACT_APP_API_URL}/api/auth`;
+`${process.env.REACT_APP_API_URL}/api/auth`;
 
 export const registerUser = async (userData) => {
   const response = await axios.post(
@@ -16,19 +16,6 @@ export const loginUser = async (userData) => {
   const response = await axios.post(
     `${API_URL}/login`,
     userData
-  );
-
-  return response.data;
-};
-
-export const getProfile = async (token) => {
-  const response = await axios.get(
-    `${API_URL}/profile`,
-    {
-      headers: {
-        Authorization: `Bearer ${token}`
-      }
-    }
   );
 
   return response.data;
